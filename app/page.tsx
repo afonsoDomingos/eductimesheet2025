8dc3a6c4adfe46d6a4b1038b66af643b4b832135
afonsoDomingos <karinganastudio23@gmail.com>
2025-07-28 10:33:21 +0200

    v4.0.0 - 2025-07-28 Atualizando o projecto para Aroma Natural

--- app/page.tsx
+++ app/page.tsx
@@ -710,7 +710,7 @@ export default function PlataformaAtividades() {
           <div className="flex flex-col sm:flex-row justify-between items-center h-auto sm:h-16 gap-2 sm:gap-0">
             <div className="flex items-center mb-2 sm:mb-0">
               <Activity className="h-8 w-8 text-blue-600 mr-3" />
-              <h1 className="text-xl font-semibold text-gray-900">Plano de Actividades MUV</h1>
+              <h1 className="text-xl font-semibold text-gray-900">Plano de Actividades Aroma Natural</h1>
             </div>
             <nav className="flex flex-col sm:flex-row w-full sm:w-auto gap-2 sm:gap-8 items-center justify-center">
               <button
